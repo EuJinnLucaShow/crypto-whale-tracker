071e95e49a8a986703bccdaa943c1b7ea87cb41c
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2026-02-22 14:58:58 +0200

    build(api): achieve zero vulnerabilities with Chainguard

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,11 @@ import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +19,6 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.enableShutdownHooks();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
